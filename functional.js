20a12aa2ee37afdcc2b3d6b886327967f1b250fc
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2021-06-09 21:43:50 +1000

    6.3 reduce & exercises 6.3.4

--- functional.js
+++ functional.js
@@ -44,3 +44,64 @@ function functionalSingles(elements) {
 console.log(imperativeSingles(states));
 
 console.log(functionalSingles(states));
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function(n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+function functionalSum(elements) {
+  return elements.reduce((total, n) => { return total += n; });
+}
+
+console.log(functionalSum(numbers));
+
+//lengths imperative solution
+
+function imperativeLength(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  })
+  return lengths;
+}
+
+console.log(imperativeLength(states));
+
+//lengths functional solution
+
+function functionalLength(elements) {
+  return elements.reduce((lengths, element) => {
+                            lengths[element] = element.length;
+                            return lengths;
+                            },
+                            {}
+                          );
+}
+
+console.log(functionalLength(states));
+
+// execrise reduce 6.3.4.1 product
+
+let items = [2,2,4];
+function imperativeProduct(elements) {
+  let product = 1;
+  elements.forEach(function(element) {
+    product *= element;
+  });
+  return product;
+}
+
+console.log(imperativeProduct(items));
+
+function functionalProduct(elements) {
+  return items.reduce((product, element) => { return product *= element},1);
+}
+
+console.log(functionalProduct(items));

bba58c1df61a9908bd03496352f9bebb75d4ed6f
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2021-06-11 14:55:15 +1000

    exercise 7.2.1 prototypes

--- palindrome.js
+++ palindrome.js
@@ -3,12 +3,46 @@ function reverse(string) {
   return Array.from(string).reverse().join("");
 }
 
-//Returns true for a palindrome, false otherwise
-function palindrome(string) {
-  let processedContent =  string.toLowerCase();
-  return processedContent === reverse(processedContent);
-}
+
 
 function emailParts(email) {
   return email.split("@")
 }
+
+
+//define a Phrase Object
+function Phrase(content) {
+  this.content = content;
+
+  //Returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  //Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+
+  this.louder = function louder() {
+    return this.processedContent().toUpperCase();
+  }
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+}
+
+// defines a transalted object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  //Returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
